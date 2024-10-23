2db574ecfdc24c0a04312908a05fabc85628f1ea
Pavel Sukhodoev <xcyxoux@gmail.com>
2024-10-23 13:09:22 +0300

    make customer fully optional

--- yookassa/common/customer.go
+++ yookassa/common/customer.go
@@ -2,8 +2,8 @@ package yoocommon
 
 type Customer struct {
 	// all optional
-	FullName string `json:"full_name"`
-	INN      string `json:"inn"`
-	Phone    string `json:"phone"`
-	Email    string `json:"email"`
+	FullName string `json:"full_name,omitempty"`
+	INN      string `json:"inn,omitempty"`
+	Phone    string `json:"phone,omitempty"`
+	Email    string `json:"email,omitempty"`
 }
